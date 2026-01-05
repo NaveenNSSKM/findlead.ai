813320821189b900799e18aca5fbfa1eff981b7a
NSSKM <kirubasumathi45@gmail.com>
2026-01-05 18:10:48 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://api.fontshare.com" />
+        <link rel="preconnect" href="https://cdn.fontshare.com" crossOrigin="anonymous" />
         <link href="https://api.fontshare.com/v2/css?f%5B%5D=clash-display@400,500,600,700&display=swap" rel="stylesheet" />
       </head>
       <body
@@ -31,6 +33,6 @@ export default function RootLayout({
       >
         {children}
       </body>
-    </html>
+    </html >
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,12 @@ const nextConfig = {
         buildActivity: false,
         appIsrStatus: false,
     },
+    images: {
+        formats: ['image/avif', 'image/webp'],
+    },
+    compiler: {
+        removeConsole: process.env.NODE_ENV === "production",
+    },
 };
 
 export default nextConfig;
