4a349e9f7cdeddefebbac7556197bb6c4916a220
NSSKM <kirubasumathi45@gmail.com>
2026-01-06 16:28:56 +0530

    up-towards

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,13 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://cdn.fontshare.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://cdn.fontshare.com" />
+        <link
+          rel="preload"
+          href="https://cdn.fontshare.com/wf/BFBSY7LX5W2U2EROCLVVTQP4VS7S4PC3/IIUX4FGTMD2LK2VWD3RVTAS4SSMUN7B5/53RZKGODFYDW3QHTIL7IPOWTBCSUEZK7.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
       </head>
       <body
         className={`${openSans.variable} antialiased bg-white text-[#121212] overflow-x-hidden`}
